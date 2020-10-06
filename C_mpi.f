b8b66e951a79b19bbe33d508451f31196660cdc1
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-10-06 10:28:02 +0200

    run on one processor - commented out

--- C_mpi.f
+++ C_mpi.f
@@ -3,4 +3,5 @@ c     mpi variables
 c-----------------------------------------------------------------------
        include "mpif.h"
        PARAMETER (nPh=3,nPv=6,nprocs=nPh*nPv)
+cKB       PARAMETER (nPh=1,nPv=1,nprocs=nPh*nPv)
        common /mpivar/  myid,ierr,status(100,MPI_STATUS_SIZE)
