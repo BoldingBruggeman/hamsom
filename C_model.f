903bfdaac8bd02a2ff2c0c47e01135d92b71c109
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-10-28 15:27:04 +0100

    use C_nbio_fabm.f to specify bio sizes

--- C_model.f
+++ C_model.f
@@ -5,8 +5,8 @@ c______ Model north_b _________________
       parameter (khor=8216,ndrei=82108,kasor=8106)    ! 2d->1d, 3d->1d, 2d->1d for SOR
       parameter (lrp=55,iranz=2,lvrp=840)             ! liquid boudaries size
 c      parameter (nbio=23)  !biological parameters, see C_bio.f
-      parameter (nbio=14+2) !biological parameters + T,S, see C_bio.f
-      parameter (nsed=3)  !biological parameters, see C_bio.f
+c      parameter (nbio=2) !biological parameters + T,S, see C_bio.f
+c      parameter (nsed=0)  !biological parameters, see C_bio.f
 c Attention!!! ngro must be recalculated depending on model domain geometry by
 c              ngro = (max(m*(ilo*20+10),(kasor*8)))
       parameter (ngro=m*(ilo*20+10)) ! north_b
---
+++ C_nbio_fabm.f
@@ -0,0 +1,6 @@
+*     c_model.f
+
+c______ Model north_b _________________
+      parameter (nbio=14+2) !biological parameters + T,S, see C_bio.f
+      parameter (nsed=3)  !biological parameters, see C_bio.f
+c_____   north_b _________________
