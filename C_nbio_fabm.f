1595ae4e74117da5bd05726dfe5ec6cc60d1ac93
Karsten Bolding <karsten@bolding-bruggeman.com>
2020-11-25 08:47:31 +0100

    adjusted nbio to reflect fabm.yaml file

--- C_nbio_fabm.f
+++ C_nbio_fabm.f
@@ -1,6 +1,7 @@
 *     c_model.f
 
 c______ Model north_b _________________
-      parameter (nbio=14+2) !biological parameters + T,S, see C_bio.f
+c      parameter (nbio=14+2) !biological parameters + T,S, see C_bio.f
+      parameter (nbio=12+2) !biological parameters + T,S, see C_bio.f
       parameter (nsed=3)  !biological parameters, see C_bio.f
 c_____   north_b _________________
