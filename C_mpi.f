171d58282258552df80a93700eec022a519a08aa
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-10-28 10:51:35 +0200

    updates by Ute Daewel

--- C_mpi.f
+++ C_mpi.f
@@ -3,5 +3,5 @@ c     mpi variables
 c-----------------------------------------------------------------------
        include "mpif.h"
        PARAMETER (nPh=3,nPv=6,nprocs=nPh*nPv)
-cKB       PARAMETER (nPh=1,nPv=1,nprocs=nPh*nPv)
+c       PARAMETER (nPh=2,nPv=2,nprocs=nPh*nPv)
        common /mpivar/  myid,ierr,status(100,MPI_STATUS_SIZE)
