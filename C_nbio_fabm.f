df27bb604ba7f60e062986d9cca219db367e6480
Karsten Bolding <karsten@bolding-bruggeman.com>
2023-02-06 14:54:28 +0100

    allow Ute to add stuff on the HAMSOM side

--- C_nbio_fabm.f
+++ C_nbio_fabm.f
@@ -2,6 +2,7 @@
 
 c______ Model north_b _________________
 c      parameter (nbio=14+2) !biological parameters + T,S, see C_bio.f
-      parameter (nbio=12+2) !biological parameters + T,S, see C_bio.f
+!KB      parameter (nbio=12+2) !biological parameters + T,S, see C_bio.f
+      parameter (nbio=13+2) !biological parameters + T,S, see C_bio.f
       parameter (nsed=3)  !biological parameters, see C_bio.f
 c_____   north_b _________________
